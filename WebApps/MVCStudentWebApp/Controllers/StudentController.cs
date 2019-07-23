9c3e0a9f1a49884d579f3293e4da298a77326f89
Humaniuc <humancoria@gmail.com>
2019-07-23 12:39:36 +0300

    StudentWebApp

---
+++ WebApps/MVCStudentWebApp/Controllers/StudentController.cs
@@ -0,0 +1,68 @@
+﻿using MVCStudentWebApp.Models.StudentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStudentWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static readonly List<Student> students = new List<Student>
+            {
+                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+            };
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(students);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var student = students.Where(s => s.StudentId == id).FirstOrDefault();
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Edit", std);
+            }
+            else
+            {
+                var editedStudent = students.Find(s => s.StudentId == std.StudentId);
+                editedStudent.StudentName = std.StudentName;
+                editedStudent.Age = std.Age;
+                editedStudent.isNewlyEnrolled = std.isNewlyEnrolled;
+                editedStudent.Password = std.Password;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var student = students.Where(s => s.StudentId == id).FirstOrDefault();
+            return View(student);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            students.Remove(students.Find(s => s.StudentId == id));
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApps/MVCStudentWebApp/Models/StudentModel/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCStudentWebApp.Models.StudentModel
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Display(Name="Name")]
+        [Required]
+        public string StudentName { get; set; }
+
+        [Range(5, 50)]
+        public int Age { get; set; }
+
+        [Display(Name = "Boboc?")]
+        public bool isNewlyEnrolled { get; set; }
+        public string Password { get; set; }
+    }
+}
